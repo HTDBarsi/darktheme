7b5ce29b818673dd99a0c927ff557a4d4e86e794
HTDBarsi <57287841+HTDBarsi@users.noreply.github.com>
2022-03-17 11:14:00 +0100

    Create darktheme.ps1

---
+++ darktheme.ps1
@@ -0,0 +1,2 @@
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" SystemUsesLightTheme -Value 0
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" AppsUseLightTheme -Value 0
