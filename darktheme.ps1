b77565edc5ec4bc927fef54552151967c70341ef
HTDBarsi <57287841+HTDBarsi@users.noreply.github.com>
2022-03-17 11:29:45 +0100

    Update darktheme.ps1

--- darktheme.ps1
+++ darktheme.ps1
@@ -1,2 +1,3 @@
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" SystemUsesLightTheme -Value 0
 Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" AppsUseLightTheme -Value 0
+Set-ItemProperty "HKCU:\Control Panel\Desktop" PaintDesktopVersion -Value 0
